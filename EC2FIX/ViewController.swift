792f5351cf325e3348e72818e22d1d207d73d6b6
Aswin CJ <aswincj4@gmail.com>
2017-07-09 21:30:15 +0530

    first commit + skeleton designs

--- EC2FIX/ViewController.swift
+++ EC2FIX/ViewController.swift
@@ -9,14 +9,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    func UIColorFromRGB(rgbValue: UInt) -> UIColor {
+        return UIColor(
+            red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
+            green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
+            blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
+            alpha: CGFloat(1.0)
+        )
+    }
+    
+  
+ 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
         let statWindow = UIApplication.shared.value(forKey:"statusBarWindow") as! UIView
         let statusBar = statWindow.subviews[0] as UIView
-        statusBar.backgroundColor = UIColor.red //your required color
+        statusBar.backgroundColor = UIColorFromRGB(rgbValue: 0x0078ff) //your required color
     }
 
     override func didReceiveMemoryWarning() {
